7f6eacb7c850b3e85f68081a1c48b6a1a3e87f88
Joe Corcoran <joe@corcoran.io>
2017-05-04 11:49:45 +0200

    Add deploy setup

---
+++ ci/before_deploy.sh
@@ -0,0 +1,29 @@
+set -ex
+
+main() {
+    local src=$(pwd) \
+          stage=
+
+    case $TRAVIS_OS_NAME in
+        linux)
+            stage=$(mktemp -d)
+            ;;
+        osx)
+            stage=$(mktemp -d -t tmp)
+            ;;
+    esac
+
+    test -f Cargo.lock || cargo generate-lockfile
+
+    cross build --release --target $TARGET
+
+    cp target/$TARGET/release/redactor $stage/
+
+    cd $stage
+    tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
+    cd $src
+
+    rm -rf $stage
+}
+
+main
