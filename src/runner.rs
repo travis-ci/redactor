6ffe38f754fb6296dbcd52f25ebdf24f8558ffd6
Joe Corcoran <joe@corcoran.io>
2017-05-08 16:51:15 +0200

    Test writing of script

--- src/runner.rs
+++ src/runner.rs
@@ -2,8 +2,14 @@ use exec;
 use siphasher::sip::SipHasher;
 use std::fs::{self, File};
 use std::hash::Hasher;
-use std::io::{Read, Write};
+use std::io::{self, Read, Write};
 use std::os::unix::fs::PermissionsExt;
+
+#[derive(Debug)]
+pub enum Error {
+    IoError(io::Error),
+    ExecError(exec::Error)
+}
 
 pub struct Runner<'a> {
     cmd: &'a str,
@@ -23,36 +29,33 @@ impl<'a> Runner<'a> {
         }
     }
 
-    fn script_name(&self) -> String {
-        format!("{}.sh", self.hash)
+    pub fn exec(&mut self) -> Error {
+        match self.write_script() {
+            Ok(_) => {
+                let error = exec::Command::new("bash").arg(&self.script_name()).exec();
+                Error::ExecError(error)
+            },
+            Err(error) => Error::IoError(error)
+        }
     }
 
-    fn status_name(&self) -> String {
-        format!("{}.status", self.hash)
-    }
-
-    pub fn exec(&mut self) -> exec::Error {
-        // insert cmd into bash script
+    pub fn write_script(&mut self) -> io::Result<()> {
         let script = format!("#!/usr/bin/env bash\n{}\nprintf $? > {}.status", self.cmd, self.hash);
-        let mut file = match File::create(&self.script_name()) {
-            Ok(file) => file,
-            _ => panic!("Could not create bash script")
-        };
-        let _ = file.write_all(script.as_bytes());
-
-        // set chmod +x
-        let meta = file.metadata().unwrap();
-        let mut perm = meta.permissions();
-        perm.set_mode(0o744);
-
-        // exec
-        exec::Command::new("bash").arg(&self.script_name()).exec()
+        match File::create(&self.script_name()) {
+            Ok(mut file) => {
+                let _ = file.write_all(script.as_bytes());
+                let meta = file.metadata().unwrap();
+                let mut perm = meta.permissions();
+                perm.set_mode(0o744);
+                Ok(())
+            },
+            Err(error) => Err(error)
+        }
     }
 
     pub fn status(&mut self) -> Option<i32> {
         if self.status.is_some() { return self.status; }
 
-        // if status file exists read status, convert to int and return
         match File::open(&self.status_name()) {
             Ok(ref mut file) => {
                 let mut contents = String::new();
@@ -74,6 +77,14 @@ impl<'a> Runner<'a> {
         self.cleanup();
         status
     }
+
+    fn script_name(&self) -> String {
+        format!("{}.sh", self.hash)
+    }
+
+    fn status_name(&self) -> String {
+        format!("{}.status", self.hash)
+    }
 }
 
 #[cfg(test)]
@@ -85,6 +96,22 @@ mod test {
         let cmd = "ruby stream.rb";
         let runner = Runner::new(cmd);
         assert_eq!(2392779401433226153, runner.hash);
+        runner.cleanup();
+    }
+
+    #[test]
+    fn write_script() {
+        let cmd = "ruby stream.rb";
+        let mut runner = Runner::new(cmd);
+        let _ = runner.write_script();
+        match File::open("2392779401433226153.sh") {
+            Ok(mut file) => {
+                let mut contents = String::new();
+                let _ = file.read_to_string(&mut contents);
+                assert_eq!(String::from("#!/usr/bin/env bash\nruby stream.rb\nprintf $? > 2392779401433226153.status"), contents);
+            },
+            _ => panic!("Script not found")
+        }
         runner.cleanup();
     }
 }
